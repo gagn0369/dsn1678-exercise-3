8f4cfc9dec583c75acbbff6fd6ed128c0d951220
gagn0369 <madeleinemagnolia@gmail.com>
2014-10-02 10:47:48 -0400

    basic javascript

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var sales__details__left = $(".sales__details__left");
+
+var clickArea__left = $(".sales__left");
+
+var detailsShow = function () {
+	var currentState = sales__details__left.attr("data-state")
+	if (currentState == "active") {
+		sales__details__left.attr("data-state", "inactive");
+	} else {
+		sales__details__left.attr("data-state", "active");
+	}
+	
+};
+
+clickArea.on("click", detailsShow);
+
+var sales__details__right = $(".sales__details__right");
+
+var clickArea__right = $(".sales__right");
+
+var detailsShow = function () {
+	var currentState = sales__details__right.attr("data-state")
+	if (currentState == "active") {
+		sales__details__right.attr("data-state", "inactive");
+	} else {
+		sales__details__right.attr("data-state", "active");
+	}
+	
+};
+
+clickArea.on("click", detailsShow);
